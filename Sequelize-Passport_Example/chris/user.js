ae2096b0215352846f24564bc5b21e8578dc6b3f
Cadertea <cadertea@ncsu.edu>
2020-12-11 13:59:22 -0500

    changed the users.js file in chris folder commented out things we dont need, but might need, should all sync now to server and able to create table

--- Sequelize-Passport_Example/chris/user.js
+++ Sequelize-Passport_Example/chris/user.js
@@ -52,17 +52,17 @@ module.exports = function(sequelize, DataTypes) {
     // }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  sellers.prototype.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
+  // sellers.prototype.validPassword = function(password) {
+  //   return bcrypt.compareSync(password, this.password);
+  // };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  sellers.addHook("beforeCreate", user => {
-    user.password = bcrypt.hashSync(
-      user.password,
-      bcrypt.genSaltSync(10),
-      null
-    );
-  });
-  return User;
+  // sellers.addHook("beforeCreate", user => {
+  //   user.password = bcrypt.hashSync(
+  //     user.password,
+  //     bcrypt.genSaltSync(10),
+  //     null
+  //   );
+  // });
+  return sellers;
 };
